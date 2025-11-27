9d3d957e6d3ea9cdf0016dca349bdc180ac3ff84
Yessir664 <justinzwork23@gmail.com>
2025-11-27 00:12:55 -0500

    scroll to top

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,12 @@ import './index.css'
 import { CartProvider } from './context/CartContext'
 import { ConfigProvider } from './context/ConfigContext'
 import { ThemeProvider } from './context/ThemeContext'
+import ScrollToTop from './components/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <ConfigProvider>
         <ThemeProvider>
           <CartProvider>
